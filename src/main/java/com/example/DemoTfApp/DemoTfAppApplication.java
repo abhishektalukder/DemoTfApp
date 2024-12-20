956df486654595d13a42bac9776ebb28c5c20fe3
abhishektalukder <abhishek.talukder07@gmail.com>
2024-12-20 08:16:50 +0530

    Initial commit

---
+++ src/main/java/com/example/DemoTfApp/DemoTfAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.DemoTfApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTfAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTfAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DemoTfApp/DemoTfAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoTfApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTfAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
